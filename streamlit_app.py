95e0c769dd8c749d0b12d73bc40353e193dc3c93
THIAGO MENEZES <57110588+cloudarchitectca@users.noreply.github.com>
2023-01-02 21:11:21 -0700

    Update streamlit_app.py
    #importing pandas library, creating a DATAFRAME called my_fruit_list and loading the S3 file
    #display the dataframe on sreamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit
-
+#title
 streamlit.title('My Parents New Healthy Diner') 
-
+#creating header and the menu items
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
+#creating a second header
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#importing pandas library, creating a DATAFRAME called my_fruit_list and loading the S3 file
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list) #display the dataframe on sreamlit
